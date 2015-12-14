d8fecf509ddb69b44fe9480be95d6a827ff43b9e
Erik Humphrey <ContactErikH@gmail.com>
2015-12-13 20:20:07 -0500

    moved files

---
+++ HumphreyErik22Maze/Form1.cs
@@ -0,0 +1,35 @@
+﻿/*  ___  ___                  
+    |  \/  |                  
+    | .  . |  __ _  ____  ___ 
+    | |\/| | / _` ||_  / / _ \
+    | |  | || (_| | / / |  __/
+    \_|  |_/ \__,_|/___| \___|  */
+                          
+/* Author: Erik Humphrey
+ * Project title: Project 22 - Maze
+ * Date started: December 13th 2015
+ * Executable name: HumphreyErik22Maze.exe
+ * Description: Maze game; user must move cursor from start to finish without touching any walls
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumphreyErik22Maze
+{
+    public partial class frmMazeGame : Form
+    {
+        public frmMazeGame()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
